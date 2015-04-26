e56eeb6a41b5b06632d0a48f18a5128e2072c27c
kailevy <kai0levy@gmail.com>
2015-04-26 18:06:51 -0400

    using id3reader

--- artist.py
+++ artist.py
@@ -4,36 +4,38 @@ import operator
 from tune import Tune
 import id3reader
 
-folder_name = 'Songs_Downloads'
+folder_name = 'song_test'
 dirs = os.listdir('./' + folder_name)
 list_of_songs = []
 song_class = []
 compared_list = []
 tempos = []
+othertempos = []
 
-start = 'http://developer.echonest.com/api/v4/artist/hotttnesss?'
 
 # Converting filenames to actual song names
 for filename in dirs:
-    id3r = id3reader.Reader(folder_name+'/'+filename)
+    path = folder_name+'/'+filename
+    id3r = id3reader.Reader(path)
+    artist = id3r.getValue('performer')
+    song_name = id3r.getValue('title')
 
-    print id3r.getValue('performer')
-    # song_name = str.replace(filename, '.mp3', '')
-    # list_of_songs.append(song_name)
-    # searched = song.identify(title=song_name, filename=folder_name+'/'+filename)
-    # if len(searched) == 1:
-    #     print searched['metadata']
-    #     compared_list.append(searched[0])
-    #     artist = searched[0].artist_name
-    #     songname = searched[0].title
-    #     path = folder_name+'/'+filename
-    #     print artist, songname, path
-    #     try:
-    #         a = Tune(folder_name+'/'+filename, songname, artist)
-    #         tempos.append(a.bpm)
-    #     except RuntimeError:
-    #         pass
+    searched = song.search(title=song_name, artist=artist, results=1)
+    if len(searched) == 1:
+        compared_list.append(searched[0])
+        artist = searched[0].artist_name
+        songname = searched[0].title
+        print artist, songname, path
+        try:
+            a = track.track_from_filename(path)
+            # a = Tune(folder_name+'/'+filename, songname, artist)
+            tempos.append(a.tempo)
+        except RuntimeError:
+            pass
         
+print compared_list
+print tempos
+
 
 tempos = [i//2 if i > 161 else i for i in tempos]
 
@@ -41,13 +43,4 @@ first_run = dict(zip(compared_list, tempos))
 second_run = sorted(first_run.items(), key=operator.itemgetter(1))
 final_list = []
 
-print second_run
-
-
-# for tup in second_run:
-# 	final_list.append(tup[0])
-# print final_list
-
-# for element in final_list:
-# 	print element.audio_summary['tempo']
-
+# print second_run
