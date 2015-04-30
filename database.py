1d099b3b64db74023c7263718d2d06cee11b9e92
kailevy <kai0levy@gmail.com>
2015-04-29 22:58:09 -0400

    Added song search fields

--- database.py
+++ database.py
@@ -16,14 +16,27 @@ cur.execute("DROP TABLE IF EXISTS Songs")
 cur.execute("CREATE TABLE Songs(Id INT PRIMARY KEY AUTO_INCREMENT, \
     File_Path VARCHAR(50),\
     Artist VARCHAR(20),\
-    Title VARCHAR(20))")
+    Title VARCHAR(20),\
+    Tempo Float(4,1),\
+    Danceability FLOAT(6,6),\
+    Liveness FLOAT(6,6),\
+    Energy FLOAT(6,6))")
+
 for filename in song_dir:
     print filename
     path = 'song_test/' + filename
     id3r = id3reader.Reader(path)
     artist = id3r.getValue('performer')
     song_name = id3r.getValue('title')
-    cur.execute("INSERT INTO Songs(File_Path, Artist, Title) Value('%s', '%s', '%s')" % (path, artist, song_name))
+    echo_song = song.search(title=song_name, artist=artist, results=1)
+    if echo_song:
+        tempo = echo_song[0].audio_summary['tempo']
+        danceability = echo_song[0].audio_summary['danceability']
+        liveness = echo_song[0].audio_summary['liveness']
+        energy = echo_song[0].audio_summary['energy']
+        cur.execute("INSERT INTO Songs(File_Path, Artist, Title, Tempo, Danceability, Liveness, Energy) \
+            Value('%s', '%s', '%s', '%d', '%d', '%d', '%d')" % 
+            (path, artist, song_name, tempo, danceability, liveness, energy))
     
 cur.execute("SELECT * FROM Songs")
 rows = cur.fetchall()
