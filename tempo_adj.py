41646d1fb774013450e31b0815c7d5ac6941ffa4
kailevy <kai0levy@gmail.com>
2015-05-04 19:40:48 -0400

    Fix tempo_adj

--- tempo_adj.py
+++ tempo_adj.py
@@ -1,7 +1,7 @@
 from echonest.remix.action import render, Crossfade, Crossmatch
 from pyechonest import config
 
-def make_crossmatch(song1, song2, bars1, bars2, fade_amt):
+def make_crossmatch(song1, song2, bars1, bars2):
     crossed_song = Crossmatch( (song1, song2), (to_tuples(bars1), to_tuples(bars2)))
     return [crossed_song]
 
